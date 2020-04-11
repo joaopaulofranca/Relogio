de2704ea30620e9089c47897f06f96a5d3bfbcda
joaopaulofranca <joaopaulofrancasilva@outlook.com>
2020-04-11 20:36:18 -0300

    Finalizando cronometro

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+//------------------RELOGIO---------------------------------
 function Relogio() {
     var data = new Date();
     var hr = data.getHours();
@@ -21,8 +22,18 @@ function Relogio() {
     tempo.innerHTML = tempo_total;
 }
 setInterval(Relogio, 500);
+//-------------------------------------------------------------
+
+
+
+//--------CRONOMETRO--------------
+var cronometro = 0;
 
 function iniciarCronometro() {
+    //para aparecer um botao de cada vez
+    document.getElementById('btnIniciar').style.display = "none";
+    document.getElementById('btnZerar').style.display = "inline";
+
     var timer_horas = document.getElementById('timer_horas');
     var timer_minutos = document.getElementById('timer_minutos');
     var timer_segundos = document.getElementById('timer_segundos');
@@ -32,9 +43,39 @@ function iniciarCronometro() {
     var m = 0;
     var s = 0;
     var d = 0;
-    setInterval(function() {}, 1000);
+
+    cronometro = setInterval(function() {
+        timer_horas.innerHTML = h < 10 ? '0' + h : h;
+        timer_minutos.innerHTML = m < 10 ? '0' + m : m;
+        timer_segundos.innerHTML = s < 10 ? '0' + s : s;
+        timer_decimos.innerHTML = d < 10 ? '0' + d : d;
+
+        if (d < 9) {
+            d += 1
+        } else if (s < 59) {
+            d = 0;
+            s += 1
+        } else if (m < 59) {
+            d = 0;
+            s = 0;
+            m += 1;
+        } else if (h < 23) {
+            d = 0;
+            s = 0;
+            m = 0;
+            h += 1;
+        }
+
+    }, 100);
 }
 
 function zerarCronometro() {
+    document.getElementById('btnIniciar').style.display = "inline";
+    document.getElementById('btnZerar').style.display = "none";
 
+    clearInterval(cronometro);
+    document.getElementById('timer_horas').innerHTML = "00";
+    document.getElementById('timer_minutos').innerHTML = "00";
+    document.getElementById('timer_segundos').innerHTML = "00";
+    document.getElementById('timer_decimos').innerHTML = "00";
 }
